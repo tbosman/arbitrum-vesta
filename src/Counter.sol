91b1f3dad9b1a50da842e9620ecaeb25ee1aebcd
Thomas Bosman <tbosman@gmail.com>
2022-10-16 11:01:17 -0500

    update

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// import "submodules/vesta-protocol-v1/contracts/TroveManager.sol"
+pragma solidity ^0.8.0;
+
+// import "@vesta-protocol/contracts/TroveManager.sol";
+// import "forge-std/Vm.sol";
+import "../submodules/vesta-protocol-v1/contracts/TroveManager.sol";
 
 contract Counter {
     uint256 public number;
